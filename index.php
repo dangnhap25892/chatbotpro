6438ea1704772a6eac01a15c2c7f64819fc8d70f
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-01 15:36:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $jsonData ="{
 }";
 
   $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
     curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
     curl_exec($ch);
