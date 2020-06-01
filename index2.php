f144b7ab4396581c35f01674bdaf7f72e19de2fc
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-01 16:37:59 +0700

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,51 @@
+<?php
+
+$token = "EAADn4qwXcIQBAFHRcn1PVuyAjPrYaJbqRJZBAMPWJiVNH4IDnu2Q1zv1hNORLZBKj1MT0eYJudICNjQTaycesDiZBvtyhhOLqKuFgTapMm9Bt9tSNdPocIaC3aBcOAzHg8hUsxMrxnj9sZAQiBwVmzZBJOfu4WjgnvbU9igFuVQZDZD";
+if (isset($_REQUEST['hub_challenge']))
+{
+	$c = $_REQUEST['hub_challenge'];
+	$v = $_REQUEST['hub_verify_token'];
+}
+
+if($v =="123")
+{
+	echo $c;
+	die();
+	
+	
+}
+$input = json_decode(file_get_contents('php://input'),true);
+
+$userID = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = $input['entry'][0]['messaging'][0]['sender']['text'];
+
+//echo $userID." and ".$message;
+
+$url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
+
+$jsonData ="{
+	'messaging_type' : 'RESPONSE',
+	'recipient':{
+		'id': $userID
+	},
+	'message':{
+		'text':'hello232'
+		}
+}";
+
+sendchat($token,$jsonData);
+function sendchat($token,$jsonData)
+{
+$url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
+
+  $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_exec($ch);
+    curl_close($ch);
+}
+
+
+
+?>
