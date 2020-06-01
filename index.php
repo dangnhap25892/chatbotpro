4fe17ddfbb86db7aa61d9c5c34c0d1bf0e00c55e
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-01 16:10:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -24,9 +24,23 @@ $jsonData ="{
 	'recipient':{
 		'id': $userID
 	},
-	'message':{
-		'text':'".$message."'
-	}
+	"messaging_type": "RESPONSE",
+  "message":{
+    "text": "Pick a color:",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Red",
+        "payload":"<POSTBACK_PAYLOAD>",
+        "image_url":"http://example.com/img/red.png"
+      },{
+        "content_type":"text",
+        "title":"Green",
+        "payload":"<POSTBACK_PAYLOAD>",
+        "image_url":"http://example.com/img/green.png"
+      }
+    ]
+  }
 }";
 
   $ch = curl_init($url);
