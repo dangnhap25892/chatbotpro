d36f161dd37f41a12602ea03bd82f618399e860a
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-01 15:27:34 +0700

    Add files via upload

---
+++ chatbot.php
@@ -0,0 +1,46 @@
+<?php
+
+$token = "EAADn4qwXcIQBAFHRcn1PVuyAjPrYaJbqRJZBAMPWJiVNH4IDnu2Q1zv1hNORLZBKj1MT0eYJudICNjQTaycesDiZBvtyhhOLqKuFgTapMm9Bt9tSNdPocIaC3aBcOAzHg8hUsxMrxnj9sZAQiBwVmzZBJOfu4WjgnvbU9igFuVQZDZD";
+if (isset($_REQUEST['hub_challenge']))
+{
+	$c = $_REQUEST['hub_challenge'];
+	$v = $_REQUEST['hub_verify_token'];
+}
+
+if($v =="123")
+{
+	echo $c;
+}
+$input = json_decode(file_get_contents('php://input'),true);
+
+$userID = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = $input['entry'][0]['messaging'][0]['sender']['text'];
+
+echo $userID." and ".$message;
+
+$url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
+
+$jsonData ="{
+	'recipient':{
+		'id': $userID
+	},
+	'message':{
+		'text':'hello'
+	}
+}";
+
+  $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+
+    if(!empty($input['entry'][0]['messaging'][0]['message'])){
+    	curl_exec($ch);	
+    }
+    
+
+
+
+
+
+?>
