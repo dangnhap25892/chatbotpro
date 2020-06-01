dc6fd77f8f7e9e4205bba06a1296b8b1ae9d679b
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-01 15:19:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,32 @@ if($v =="123")
 {
 	echo $c;
 }
+$input = json_decode(file_get_contents('php://input'),true);
+
+$userID = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = $input['entry'][0]['messaging'][0]['sender']['text'];
+
+//echo $userID." and ".$message;
+
+$url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
+
+$jsonData ="{
+	'recipient':{
+		'id': $userID
+	},
+	'message':{
+		'text':'hello'
+	}
+}";
+
+  $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsondata);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_exec($ch);
+
+
+
 
 
 ?>
